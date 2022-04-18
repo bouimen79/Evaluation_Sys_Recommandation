54802dd2d9e4f3df5066854125aba79e2e65a44d
Bou_Imen <66778640+bouimen79@users.noreply.github.com>
2022-04-18 17:19:30 +0100

    Add files via upload

--- src_Version_0/src/Data_Split/Connection_Mongodb.java
+++ src_Version_0/src/Data_Split/Connection_Mongodb.java
@@ -10,23 +10,17 @@ import org.bson.Document;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static com.mongodb.client.model.Filters.eq;
 public class Connection_Mongodb {
   public static MongoDatabase database;
-    public static MongoCollection<Document> collection_Item ;
-    public static MongoCollection<Document> collection_Event ;
+
     public static  void connection_mongo(String Base_Name){
         Logger logger = Logger.getLogger("org.mongodb.driver");
         logger.setLevel(Level.SEVERE);
         //  Connexion avec Mongod
         MongoClient mongo=new MongoClient("Localhost",27017);
-       // System.out.println("Connected to the database successfully");
         // Accéder a la base de Donnée
         database = mongo.getDatabase(Base_Name);
 
-        //String a[]=d.
-        MongoCollection<Document> collection_Item = database.getCollection("Item");
-        MongoCollection<Document> collection_Event = database.getCollection("Event");
 
     }
 
@@ -35,7 +29,7 @@ public class Connection_Mongodb {
                                             String Created_at, long Item_Id, String recom,
                                             String Url, String title, String category, String text){
         //Preparing a document
-        //ItemType,Domain,CreatedAt,ItemID,Recommendable,URL,Title,category,text,keywords,categories
+        //Domain,CreatedAt,ItemID,Recommendable,URL,Title,category,text
         Document document = new Document();
 
         document.append("Domain", domain);
@@ -46,14 +40,13 @@ public class Connection_Mongodb {
         document.append("Title",title);
         document.append("category",category);
         document.append("text",text);
-
-
         //Inserting the document into the collection
         database.getCollection("Item").insertOne(document);
         // System.out.println("Document inserted successfully");
     }
     public static void ADD_Document_Event(long Id_event, String actiontype, long item_id_event, long user_id_event, String time_event, long geoUser, long deviceType){
-        //actionType,ItemID,UserId,TimeStamp,geoUser,deviceType
+        //Preparing a document
+        //Id_event,actionType,ItemID,UserId,TimeStamp,geoUser,deviceType
         Document document = new Document();
         document.append("Id_event",Id_event);
         document.append("actionType", actiontype);
@@ -62,7 +55,7 @@ public class Connection_Mongodb {
         document.append("TimeStamp",time_event);
         document.append("geoUser",geoUser);
         document.append("deviceType",deviceType);
-
+        //Inserting the document into the collection
         database.getCollection("Event").insertOne(document);
         //System.out.println("Document inserted successfully Event");
 
@@ -72,7 +65,7 @@ public class Connection_Mongodb {
     public static Document Get_Document(MongoDatabase database, String Id_Name,long Id_ , String Collection_Name){
         try {
             MongoCollection<Document> collection_Item = database.getCollection(Collection_Name);
-            System.out.println("Collection myCollection selected successfully");
+            //System.out.println("Collection myCollection selected successfully");
             BasicDBObject Query = new BasicDBObject();
             Query.put(Id_Name, Id_);
             MongoCursor<Document> cursor = collection_Item.find(Query).iterator();
